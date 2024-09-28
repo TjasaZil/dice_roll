a9d8d1cb4f57016c4a0bc4eb21d9340ac4693442
TjasaZil <tjasa.zilavec@gmail.com>
2024-09-28 19:06:20 +0200

    added custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(body: GradientContainer()),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class GradientContainer extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('First App'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'Hello World!',
-              ),
-              Text(
-                'It\'s time to learn Flutter!',
-              ),
-            ],
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+        colors: [
+          Color.fromARGB(255, 48, 15, 126),
+          Color.fromARGB(255, 55, 21, 102),
+        ],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+      )),
+      child: const Center(
+        child: Text(
+          'Hello World',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
           ),
         ),
       ),
