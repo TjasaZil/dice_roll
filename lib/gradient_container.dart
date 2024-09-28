f343c114407068656812db20d8cf193639452454
TjasaZil <tjasa.zilavec@gmail.com>
2024-09-28 19:16:30 +0200

    made widgets

---
+++ lib/gradient_container.dart
@@ -0,0 +1,22 @@
+import 'package:dice_roll/styled_text.dart';
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 48, 15, 126),
+            Color.fromARGB(255, 55, 21, 102),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        )),
+        child: const Center(
+          child: StyledText(),
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,10 @@
 import 'package:flutter/material.dart';
+import 'gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(body: GradientContainer()),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-        colors: [
-          Color.fromARGB(255, 48, 15, 126),
-          Color.fromARGB(255, 55, 21, 102),
-        ],
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-      )),
-      child: const Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
-      ),
-    );
-  }
 }
